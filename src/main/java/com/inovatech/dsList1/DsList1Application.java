102ec36116b14d399a9e995a3508819ecb139fb3
Jakson Albququerque <ipcamexterno@gmail.com>
2025-05-24 22:22:39 -0300

    Project created

---
+++ src/main/java/com/inovatech/dsList1/DsList1Application.java
@@ -0,0 +1,13 @@
+package com.inovatech.dsList1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DsList1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DsList1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/inovatech/dsList1/DsList1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.inovatech.dsList1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsList1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
